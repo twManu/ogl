9e54af0a13e6248696fd81bd13e1f25c8eeca3bd
manuchen <manuchen@gmail.com>
2018-12-09 22:10:45 +0800

    feat: cooking nv12

--- common/yuyv_to_rgb.h
+++ common/yuyv_to_rgb.h
@@ -151,9 +151,16 @@ static const char *g_vars[] = {
 	, "Utex"            //NV12 use while YUYV not
 };
 
+
+typedef enum {
+	  FMT_NV12
+	, FMT_YUYV
+} eYUV_Fmt;
+
+
 class cYUYV2RGBA : public Shader {
 protected:
-	GLuint               m_inTexture;
+	GLuint               m_inTexture[3];
 	int                  m_inWidth;
 	int                  m_inHeight;
 	//shader var
@@ -165,6 +172,7 @@ protected:
 		, ATTR_END
 	};
 	GLuint               m_vbo[ATTR_END];
+	eYUV_Fmt             m_pixfmt;
 
 public:
 	//index of shader var
@@ -174,26 +182,50 @@ public:
 		, Y_TEX
 		, U_TEX
 	};
-	cYUYV2RGBA(int inW, int inH)
+	cYUYV2RGBA(int inW, int inH, eYUV_Fmt fmt=FMT_YUYV)
 		: Shader()
-		, m_inTexture(0)
+		
 		, m_inWidth(inW)
 		, m_inHeight(inH)
-		{
+		, m_pixfmt(fmt) {
+		memset(m_inTexture, 0, sizeof(m_inTexture));
 		memset(m_varId, 0, sizeof(m_varId));
 	}
 	~cYUYV2RGBA() {
-		if( m_inTexture ) glDeleteTextures(1, &m_inTexture);
+		if( m_inTexture )
+			glDeleteTextures(sizeof(m_inTexture)/sizeof(m_inTexture[0]), m_inTexture);
 	}
 	int Init(bool linear=1) {
+		int texCount = 0;
+		GLenum texFormat[3] = { 0, 0, 0 };
 		if( !m_inWidth || !m_inHeight ) {
 			DBG(0, "input dimension error\n");
 			return 0;
 		}
-		load( (   char *)"yuyv_to_rgba"
-			, yuyv_to_rgb_vshader
-			, yuyv_to_rgb_fshader
-		);
+		switch ( m_pixfmt ) {
+		case FMT_NV12:
+			texFormat[0] = GL_RED;
+			texFormat[1] = GL_RG;
+			load( (char *)"nv12_to_rgba"
+				, yuyv_to_rgb_vshader
+				, nv12_to_rgb_fshader
+			);
+			texCount = 2;
+			break;
+
+		case FMT_YUYV:
+			texFormat[0] = GL_RG;
+			load( (char *)"yuyv_to_rgba"
+				, yuyv_to_rgb_vshader
+				, yuyv_to_rgb_fshader
+			);
+			texCount = 1;
+			break;
+		default:
+			DBG(0, "unknow yuv format\n");
+			return 0;
+		}
+		
 		const GLfloat vertex[6][3] = {
 			{ -1.0f, -1.0f, 0.0f }, /* BL */
 			{  1.0f, -1.0f, 0.0f }, /* BR */
@@ -205,20 +237,12 @@ public:
 
 		//texture from UVC is TL first
 		const GLfloat texcoord[6][2] = {
-			{ 0.0f, 1.0f }, /* TL */
-			{ 1.0f, 1.0f }, /* TR */
-			{ 0.0f, 0.0f }, /* BL */
-			{ 0.0f, 0.0f }, /* BL */
-			{ 1.0f, 1.0f }, /* TR */
-			{ 1.0f, 0.0f }  /* BR */
-#if 0
-			{ 0.0f, 0.0f }, /* BL */
-			{ 1.0f, 0.0f }, /* BR */
-			{ 0.0f, 1.0f }, /* TL */
-			{ 0.0f, 1.0f }, /* TL */
-			{ 1.0f, 0.0f }, /* BR */
-			{ 1.0f, 1.0f }  /* TR */
-#endif
+			{ 0.0f, 1.0f }, /* BL */
+			{ 1.0f, 1.0f }, /* BR */
+			{ 0.0f, 0.0f }, /* TL */
+			{ 0.0f, 0.0f }, /* TL */
+			{ 1.0f, 1.0f }, /* BR */
+			{ 1.0f, 0.0f }  /* TR */
 		};
 		 /* Allocate and assign a Vertex Array Object to our handle */
 		glGenVertexArrays(1, &m_vao);
@@ -236,7 +260,9 @@ public:
 		glVertexAttribPointer(ATTR_0_VERTEX, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 		/* Enable attribute index 0 as being used */
+		#if 0
 		glEnableVertexAttribArray(0);
+		#endif
 
 		/* Bind our second VBO as being the active buffer and storing vertex attributes (colors) */
 		glBindBuffer(GL_ARRAY_BUFFER, m_vbo[1]);
@@ -246,15 +272,10 @@ public:
 
 		/* Specify that our color data is going into attribute index 1, and contains 2 floats per vertex */
 		glVertexAttribPointer(ATTR_1_TEXCOORD, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
+		#if 0
 		/* Enable attribute index 1 as being used */
 		glEnableVertexAttribArray(1);
-#if 0
-		/* Bind attribute index 0 (coordinates) to in_Position and attribute index 1 (color) to in_Color */
-		/* Attribute locations must be setup before calling glLinkProgram. */
-		glBindAttribLocation(m_program, ATTR_0_VERTEX, "a_position");
-		glBindAttribLocation(m_program, ATTR_1_TEXCOORD, "a_texcoord");/* Bind attribute index 0 (coordinates) to in_Position and attribute index 1 (color) to in_Color */
-#endif
+		#endif
 		if( false==link() ) {
 			DBG(0, "fail to link shader\n");
 			return 0;
@@ -264,34 +285,62 @@ public:
 		for( int i=0; i<sizeof(g_vars)/sizeof(g_vars[0]); ++i )
 			m_varId[i] = glGetUniformLocation(prog, g_vars[i]);
 		//texture prepare
-		glGenTextures(1, &m_inTexture);
-		// "Bind" the newly created texture : all future texture functions will modify this texture
-		glBindTexture(GL_TEXTURE_2D, m_inTexture);
-		// Give an empty image to OpenGL ( the last "0" means "empty" )
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RG, m_inWidth, m_inHeight, 0, GL_RG, GL_UNSIGNED_BYTE, 0);
-		// Poor filtering
-		if( linear ) {
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		} else {
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glGenTextures(sizeof(m_inTexture)/sizeof(m_inTexture[0]), m_inTexture);
+		for( int i=0; i<texCount; ++i ) {
+			// "Bind" the newly created texture : all future texture functions will modify this texture
+			glBindTexture(GL_TEXTURE_2D, m_inTexture[i]);
+			// Give an empty image to OpenGL ( the last "0" means "empty" )
+			glTexImage2D(GL_TEXTURE_2D, 0, texFormat[i], m_inWidth, m_inHeight, 0, texFormat[i], GL_UNSIGNED_BYTE, 0);
+			// Poor filtering
+			if( linear ) {
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			} else {
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			}
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		}
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		return 1;
 	}
-	int Apply(const GLvoid *buf=NULL, int is709=1) {
+	int Apply(const GLvoid *buf=NULL, const GLvoid *buf2=NULL, int is709=1) {
 		use();
 		glUniform1f(m_varId[IN_WIDTH], (GLfloat) m_inWidth);
 		//color
 		glUniform1i(m_varId[COLOR709], is709);
+		//vertex
+		glBindVertexArray(m_vao);
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, m_vbo[0]);
+		glVertexAttribPointer(ATTR_0_VERTEX, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, m_vbo[1]);
+		glVertexAttribPointer(ATTR_1_TEXCOORD, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
 		//texture
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, m_inTexture);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RG, m_inWidth, m_inHeight, 0, GL_RG, GL_UNSIGNED_BYTE, buf);
-		glUniform1i(m_varId[Y_TEX], 0);
+		switch( m_pixfmt ) {
+		case FMT_YUYV:
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, m_inTexture[0]);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RG, m_inWidth, m_inHeight, 0, GL_RG, GL_UNSIGNED_BYTE, buf);
+			glUniform1i(m_varId[Y_TEX], 0);
+			break;
+		case FMT_NV12:
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, m_inTexture[0]);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, m_inWidth, m_inHeight, 0, GL_RED, GL_UNSIGNED_BYTE, buf);
+			glUniform1i(m_varId[Y_TEX], 0);
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, m_inTexture[1]);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RG, m_inWidth/2, m_inHeight/2, 0, GL_RG, GL_UNSIGNED_BYTE, buf2);
+			glUniform1i(m_varId[U_TEX], 1);
+			break;
+		}
 		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
 		return 1;
 	}
 };
